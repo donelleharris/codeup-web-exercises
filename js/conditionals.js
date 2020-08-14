c62eb3fcfce4928cdc6016d7bf430c0e5fcc3f2f
donelleharris <donelle.harris@gmail.com>
2020-08-13 22:27:56 -0500

    completed first function in conditionals.js

--- js/conditionals.js
+++ js/conditionals.js
@@ -19,6 +19,17 @@
  * Test your function by passing various string literals to it and
  * console.logging the function's return value
  */
+function analyzeColor(color){
+    if(color === 'blue'){
+        alert("blue is the color of the sky")
+    } else if(color === 'red'){
+        alert("strawberries are red")
+    } else if(color === 'cyan'){
+        alert("I don't know anything about cyan")
+    } else {
+        alert("I don't know that color")
+    }
+}
 
 // Don't change the next two lines!
 // These lines create two variables for you:
@@ -32,7 +43,8 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Pass the `randomColor` variable to your function and console.log the results.
  * You should see a different message every time you refresh the page
  */
-
+console.log(randomColor);
+analyzeColor(randomColor);
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
