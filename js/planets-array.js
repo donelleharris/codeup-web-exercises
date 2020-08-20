c86b4424c6f18242ae2667f9eceae9fbe1cc0b3b
donelleharris <donelle.harris@gmail.com>
2020-08-20 16:14:41 -0500

    class walkthrough of planets-array.js complete

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,20 +20,26 @@
     planets.unshift("The Sun");
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
+
     planets.push("Pluto");
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
-    planets.shift("The Sun");
+
+    planets.shift();
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
-    planets.pop("Pluto");
+
+    planets.pop();
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
-    console.log(planets.indexOf("Earth"));
+
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf("Earth"));
+
     planets.reverse();
     console.log("Reversing the order of the planets array.");
     console.log(planets);
+
     planets.sort();
     console.log("Sorting the planets array.");
     console.log(planets);
